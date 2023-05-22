7766b84e4a626b05d16f45951508666a132b623a
Taiwo Ijagbemi <christopherijagbemi@gmail.com>
2023-05-22 12:24:00 +0100

    added splash screen

--- app/index.js
+++ app/index.js
@@ -1,16 +1,31 @@
+import { SplashScreen } from "expo-router";
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native";
 
 export default function Page() {
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsReady(true);
+    }, 1000);
+  }, []);
+
   return (
     <SafeAreaView>
-      <Text className="text-red-500 text-2xl">Hello</Text>
-
-      <View className="grid grid-cols-2">
-        <View className="bg-green-400 h-full w-full">
-
+      {isReady ? (
+        <View className="grid grid-cols-2">
+          <View className="bg-green-400 h-full w-full">
+            <Text>Test here</Text>
+          </View>
         </View>
-      </View>
+      ): (
+        <SplashScreen />
+      )
+      }
+
+
     </SafeAreaView>
   );
 }
