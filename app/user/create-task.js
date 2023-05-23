1d40af8921a12c16b95a19c264738bbaa926c00f
Taiwo Ijagbemi <christopherijagbemi@gmail.com>
2023-05-23 17:30:56 +0100

    update create task

--- app/user/create-task.js
+++ app/user/create-task.js
@@ -1,19 +1,50 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, TextInput, ScrollView } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { ScrollView } from 'react-native-gesture-handler'
-import { Stack } from 'expo-router'
+import { Stack, useRouter } from 'expo-router'
 import { Ionicons } from '@expo/vector-icons';
 
+
+const HeaderLeft = () => {
+    const router = useRouter();
+    return (
+        <TouchableOpacity onPress={() => router.back()} >
+            <Ionicons name="chevron-back-outline" size={30} color="black" />
+        </TouchableOpacity>
+    )
+}
+
 const CreateTask = () => {
-  return (
+    return (
     <SafeAreaView>
         <Stack.Screen options={{  
             headerTitle: "Create Task",
-            headerLeft: () => (<Ionicons name="chevron-back-outline" size={24} color="black" />),
+            headerLeft: () => (<HeaderLeft/>),
         }} />
-        <ScrollView>
-            <Text>Hey</Text>
+
+        <ScrollView className="px-2">
+            <View className="flex flex-col">
+                <Text className="text-lg">Email</Text>
+                <TextInput 
+                    className="w-full h-12 bg-gray-200 rounded-lg text-2xl px-2"
+                    // value={email}
+                    // onChangeText={(text) => setEmail(text)}
+                    keyboardType='email-address'
+                    secureTextEntry={false}
+                />
+            </View>
+
+            <View className="flex flex-col mt-5">
+                <Text className="text-lg">Date</Text>
+                <TextInput 
+                    className="w-full h-12 bg-gray-200 rounded-lg text-2xl px-2"
+                    // value={email}
+                    // onChangeText={(text) => setEmail(text)}
+                    keyboardType='email-address'
+                    secureTextEntry={false}
+                />
+            </View>
+
         </ScrollView>
     </SafeAreaView>
   )
